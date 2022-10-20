0028afeda717fb19c8d642329423918c9f536362
Guac <guac@guacsenpai.com>
2022-10-19 19:03:55 -0500

    Add files via upload

--- src/components/TwitterPanel.tsx
+++ src/components/TwitterPanel.tsx
@@ -10,18 +10,26 @@ export const TwitterPanel = () => {
 
     function urlify(text: string) {
         const urlRegex = /(https?:\/\/[^\s]+)/g;
-        return text.replace(urlRegex, (url) => '<a style="color:#970e0f" href="' + url + '">' + url + '</a>');
+        return text.replace(
+            urlRegex,
+            (url) =>
+                '<a style="color:#970e0f" target="_blank" href="' +
+                url +
+                '" rel="noopener noreferrer">' +
+                url +
+                '</a>'
+        );
     }
 
     useEffect(() => {
-        fetch('https://api.onlyfands.net/posts', {
+        fetch('https://cdn.otkdata.com/api/stream/esfandtv', {
             method: 'GET',
             mode: 'cors',
         })
             .then((response) => response.json())
             .then(
                 (result) => {
-                    setTweet(result.data);
+                    setTweet(result.data[0].twitter);
                     setIsLoaded(true);
                 },
                 (error) => setIsLoaded(true)
@@ -60,12 +68,12 @@ export const TwitterPanel = () => {
                     <Typography sx={{ fontSize: 14, color: 'white' }} gutterBottom>
                         <div
                             dangerouslySetInnerHTML={{
-                                __html: DOMPurify.sanitize(urlify(tweet[0].text)),
+                                __html: DOMPurify.sanitize(urlify(tweet.text)),
                             }}
                         ></div>
                     </Typography>
                     <Box>
-                        <Link href={tweet[0].url} target="_blank" rel="noopener noreferrer">
+                        <Link href={tweet.url} target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faExternalLinkAlt} style={{ float: 'right' }} />
                         </Link>
                     </Box>
