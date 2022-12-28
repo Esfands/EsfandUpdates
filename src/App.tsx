753a3248e9d4b5b198b5997571f5652f68a393c1
Sdyess <skyler.dyess@hey.com>
2022-12-28 03:16:22 -0600

    update app fetch to use abort signals

--- src/App.tsx
+++ src/App.tsx
@@ -13,37 +13,59 @@ function App() {
     const [scheduleFlag, setScheduleFlag] = useState<boolean>(false);
 
     useEffect(() => {
-        fetch('https://flags.otkdata.com/api/flags/scheduleflag', {
-            method: 'GET',
-            mode: 'cors',
-        })
-            .then((response) => response.json())
-            .then(
-                (result) => {
-                    const flagValue = result.data[0].enabled;
-                    setScheduleFlag(flagValue);
-                },
-                (error) => {
-                    console.log('There was an error loading the schedule flag');
-                    setScheduleFlag(false);
-                }
-            );
+        const flagController = new AbortController();
+        const flagSignal = flagController.signal;
 
-        fetch('https://cdn.otkdata.com/api/stream/esfandtv', {
-            method: 'GET',
-            mode: 'cors',
-        })
-            .then((response) => response.json())
-            .then((result) => {
+        const streamController = new AbortController();
+        const streamSignal = streamController.signal;
+
+        const fetchScheduleFlag = async () => {
+            try {
+                const response = await fetch('https://flags.otkdata.com/api/flags/scheduleflag', {
+                    method: 'GET',
+                    mode: 'cors',
+                    signal: flagSignal,
+                });
+
+                const jsonResponse = await response.json();
+                const flagValue = jsonResponse.data[0].enabled;
+                setScheduleFlag(flagValue);
+            } catch {
+                console.log('Error occured while checking schedule feature flag.');
+            }
+        };
+
+        const fetchStreamData = async () => {
+            try {
+                const response = await fetch('https://cdn.otkdata.com/api/stream/esfandtv', {
+                    method: 'GET',
+                    mode: 'cors',
+                    signal: streamSignal,
+                });
+
+                const jsonResponse = await response.json();
+                const boxArt = jsonResponse.data[0].twitch.boxArt;
                 const body = document.querySelector('body');
                 if (body != null) {
                     body.style.background = `linear-gradient(0deg, rgba(16,19,22,1) 0%, rgba(39,38,44,0) 100%), url(${buildImageUrl(
-                        result.data[0].twitch.boxArt,
+                        boxArt,
                         '500',
                         '410'
                     )}`;
                 }
-            });
+            } catch {
+                console.log('Error occured while retrieving background image for box art.');
+            }
+        };
+
+        fetchScheduleFlag();
+        fetchStreamData();
+
+        return () => {
+            flagController.abort();
+            streamController.abort();
+            setScheduleFlag(false);
+        };
     }, []);
 
     if (scheduleFlag) {
